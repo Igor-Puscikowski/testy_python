18bac41da375e5f4f45bbc50c43c215ad0c014ac
igorStudia <igorpuscikowskibiznes@gmail.com>
2025-02-04 15:06:10 +0100

    feature 3 done

--- bank.py
+++ bank.py
@@ -5,4 +5,8 @@ class Bank:
         self.last_name = last_name
         self.pesel = pesel
         self.balance = 0
-
+        pesel = str(pesel)
+        if len(pesel) != 11:
+            self.pesel = "pesel niepoprawny"
+        else:
+            self.pesel = ""
--- test_bank.py
+++ test_bank.py
@@ -6,5 +6,21 @@ class TestBank(unittest.TestCase):
         acc1 = Bank("Igor","Puscik",20302)
         self.assertEqual(acc1.balance,0)
 
+    def test_pesel_10(self):
+        acc2 = Bank("kuba","kowal",1234567890)
+        self.assertEqual(acc2.pesel,"pesel niepoprawny")
+
+    def test_pesel_12(self):
+        acc2 = Bank("kuba", "kowal", 123456789012)
+        self.assertEqual(acc2.pesel, "pesel niepoprawny")
+
+    # ten powinien byc faild co oznacz ze jest gitara pesel
+    def test_pesel_11(self):
+        acc2 = Bank("kuba", "kowal", 12345678901)
+        self.assertEqual(acc2.pesel, "pesel poprawny")
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
