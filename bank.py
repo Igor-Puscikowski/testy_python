21b0aa94cac54ebed5711fad657855d37796d3bb
igorStudia <igorpuscikowskibiznes@gmail.com>
2025-02-04 10:47:56 +0100

    feature_2 done

--- bank.py
+++ bank.py
@@ -1,7 +1,8 @@
 
 class Bank:
-    def __init__(self, name, last_name):
+    def __init__(self, name, last_name, pesel ):
         self.name = name
         self.last_name = last_name
+        self.pesel = pesel
         self.balance = 0
 
--- test_bank.py
+++ test_bank.py
@@ -3,7 +3,8 @@ from bank import Bank
 
 class TestBank(unittest.TestCase):
     def test_initial_balance(self):
-        acc1 = Bank("Igor","Puscik")
+        acc1 = Bank("Igor","Puscik",20302)
         self.assertEqual(acc1.balance,0)
+
 if __name__ == '__main__':
     unittest.main()
